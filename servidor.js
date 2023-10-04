c53c7b2095e02869dd5f36eeacec72021e45cebf
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2023-10-04 15:44:29 -0500

    task list in server

--- servidor.js
+++ servidor.js
@@ -4,12 +4,17 @@ const chalk = require('chalk');
 const port = 8000;
 const host = 'localhost';
 
+const taskList = [
+    {Tarea: 'Preparar el desayuno'},
+    {Tarea: 'Preparar el estudio de trabajo'},
+    {Tarea: 'Empezar a crear un proyecto con node'},
+    {Tarea: 'Crear un servidor web'},
+    {Tarea: 'Crear una lista de tareas en el servidor'}
+]
+
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ 
-        nombre: 'Kevin Andres Bolanos N.', 
-        mensaje: 'Hola, estoy creando un servidor con node'
-    }));
+    res.end(JSON.stringify(taskList));
 });
 
 server.listen(port, host, () => {
