d7d841d5e4e51e07065be833adfa8873bde0ed4b
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2023-10-04 15:46:33 -0500

    adding-descriptions-id-completed

--- servidor.js
+++ servidor.js
@@ -5,11 +5,11 @@ const port = 8000;
 const host = 'localhost';
 
 const taskList = [
-    {Tarea: 'Preparar el desayuno'},
-    {Tarea: 'Preparar el estudio de trabajo'},
-    {Tarea: 'Empezar a crear un proyecto con node'},
-    {Tarea: 'Crear un servidor web'},
-    {Tarea: 'Crear una lista de tareas en el servidor'}
+    { id: '1', description: 'Preparar el desayuno', completed: true},
+    { id: '2', description: 'Preparar el estudio de trabajo',completed: true},
+    { id: '3', description: 'Empezar a crear un proyecto con node', completed: false},
+    { id: '4', description: 'Crear un servidor web', completed: true},
+    { id: '5', description: 'Crear una lista de tareas en el servidor', completed: false }
 ]
 
 const server = http.createServer((req, res) => {
